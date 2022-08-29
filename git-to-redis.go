ada724ce5d10a8cc30be5f0e8276ea1c1c5cf989
Constantine A. Murenin <cnst++@FreeBSD.org>
2022-08-29 18:01:21 -0500

    fields are now separated

--- git-to-redis.go
+++ git-to-redis.go
@@ -8,7 +8,7 @@ import (
 
 func convert() {
 	cmd := exec.Command("/usr/bin/git",
-		"log", "-n3", "--name-only", "--grep=Sponsored")
+		"log", "-n5", "--name-only", "--grep=Sponsored")
 	cmd.Dir = "../../freebsd/freebsd-src/"
 	//cmd.Dir = "/home/cnst/github.com/freebsd/freebsd-src/"
 
@@ -18,15 +18,38 @@ func convert() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("%s\n", output)
+	//fmt.Printf("%s\n", output)
 
 	lines := bytes.Split(output, []byte("\n"))
 	fmt.Printf("\noutput byte length: %v; number of lines: %v\n\n", len(output), len(lines))
 
+	var comm []byte
+	var spon []byte
+	var path [][]byte
+	
 	for i, s := range lines {
 		//fmt.Printf("%v:%s\n", i, s)
-		if bytes.HasPrefix(s, []byte("commit")) {
+		if bytes.HasPrefix(s, []byte("commit ")) {
 			fmt.Printf("new commit at line %v: %s\n", i, s)
+			nc := bytes.Split(s, []byte(" "))
+			fmt.Printf("\n old:%s\n%s\n{%s}\n, new:%s\n", comm, spon, path, nc[1])
+			comm = nc[1]
+			spon = nil
+			path = nil
+		} else if bytes.HasPrefix(s, []byte("Author")) {
+			//auth = s
+		} else if bytes.HasPrefix(s, []byte("Date")) {
+			//date = s
+		} else if bytes.HasPrefix(s, []byte("    Sponsored")) {
+			spon = s
+		} else if bytes.HasPrefix(s, []byte("    ")) {
+			continue
+		} else if len(s) > 0 {
+			path = append(path, s)
+			for _, ss := range path {
+				fmt.Printf("%s\n", ss)
+			}
+			fmt.Println("")
 		}
 	}
 
